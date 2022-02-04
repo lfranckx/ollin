11593a8d51a48c22e6b8d17bb2071185bc46e0db
Bowie <lfranckx@thinkful.com>
2022-02-03 18:56:29 -0600

    implement formik form validation with contact emailjs form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,75 +1,111 @@
 import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
-// import { Formik, Form, Field, ErrorMessage } from 'formik';nppm run
+import * as emailjs from 'emailjs-com';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-export default function Contact() {
-    const [messageSent, handleMessageSent] = useState(false);
-
-    function sendEmail(e) {
-        e.preventDefault();
-
-        emailjs.sendForm("service_a9og5hl", "template_9uozpwi", e.target, "user_4ZnH44kohKcJmQhnL2VGX")
+const Contact = () => {
+    const [messageSuccess, toggleMessageSuccess] = useState(false);
+    const [buttonState, handleButtonState] = useState('SEND');
+    const [message, handleMessage] = useState('');
+    
+    const serviceNum = process.env.REACT_APP_SERVICE_NUM;
+    const templateNum = process.env.REACT_APP_TEMPLATE_NUM;
+    const userId = process.env.REACT_APP_USER_ID;
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+    
+    const submitForm = (values) => {
+        handleButtonState('SENDING')
+        try {
+            emailjs.send(serviceNum, templateNum, values, userId)
             .then(res => {
-                handleMessageSent(true);
-            })
-            .catch(err => console.log(err));
+                    toggleMessageSuccess(true);
+                    handleButtonState('MESSAGE SENT');
+                }
+            )
+        } catch (error) {
+            handleMessage(error.message);
+        }
     }
+
+    const contactFormSchema = Yup.object().shape({
+        name: Yup.string().min(2, '* Name is too short').max(20, "* 20 maximum characters").required('* Required'),
+        email: Yup.string().email("* Invalid email").required("* Required"),
+        phone: Yup.string().matches(phoneRegExp, `* This doesn't look like a phone number`).max(10, '* Phone number is too long').required("* Required"),
+        message: Yup.string().min(2, "* Message is too short").max(120, "* 120 maximum characters").required("* Required"),
+    })
 
     return (
         <>
-            <form id='contact-form' onSubmit={sendEmail}>
-                <div className='flex'>
-                    <div className="label">
-                        <label htmlFor="name" id='name'></label>
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder='NAME'
-                        />
+            <Formik 
+                initialValues={{ name: "", email: "", phone: "", message: ""}} 
+                validationSchema={contactFormSchema}
+                onSubmit={submitForm}
+            >
+                <Form id='contact-form'>
+                    <div className='flex'>
+                        
+                        <div className="label">
+                            <label htmlFor="name" id='name'></label>
+                        </div>
+                        <div>
+                            <Field
+                                name="name"
+                                placeholder='NAME'
+                            />
+                        </div>
+                        <div>
+                            <ErrorMessage component="div" className='error' name='name' />
+                        </div>
+                        
+                        <div className="label">
+                            <label htmlFor="email" id='email'></label>
+                        </div>
+                        <div>
+                            <Field
+                                name="email"
+                                placeholder="EMAIL"
+                            />
+                        </div>
+                        <div>
+                            <ErrorMessage component="div" className='error' name='email' />
+                        </div>
+
+                        <div className="label">
+                            <label htmlFor="phone" id='phone'></label>
+                        </div>
+                        <div>
+                            <Field
+                                name="phone"
+                                placeholder='PHONE'
+                            />
+                        </div>
+                        <div>
+                            <ErrorMessage component="div" className='error' name='phone' />
+                        </div>
+
+                        <div className="label">
+                            <label htmlFor="message" id='message'></label>
+                        </div>
+                        <div>
+                            <Field
+                                name="message"
+                                placeholder='MESSAGE'
+                            />
+                        </div>
+                        <div>
+                            <ErrorMessage component="div" className='error' name='message' />
+                        </div>
                     </div>
 
-                    <div className="label">
-                        <label htmlFor="email" id='email'></label>
-                    </div>
-                    <div>
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="EMAIL"
-                        />
-                    </div>
+                    <button className="btn" type="submit">
+                        {buttonState}
+                    </button>    
+                </Form>
+            </Formik>
 
-                    <div className="label">
-                        <label htmlFor="phone" id='phone'></label>
-                    </div>
-                    <div>
-                        <input
-                            type="tel"
-                            name="phone"
-                            placeholder='PHONE'
-                        />
-                    </div>
+            {message && <div className='message'>{message}</div>}
 
-                    <div className="label">
-                        <label htmlFor="message" id='message'></label>
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            name="message"
-                            placeholder='MESSAGE'
-                        />
-                    </div>
-                </div>
-
-                <button className="btn" type="submit">
-                    SEND
-                </button>                        
-            </form>
-
-            <div className={ messageSent ? 'ty-wrap active' : 'ty-wrap' } >
+            <div className={ messageSuccess ? 'ty-wrap active' : 'ty-wrap' } >
                 <p>
                     Thank you, we will be in touch shortly. In the meantime you can follow us <a href='https://www.instagram.com/ollinsalon/?hl=en' target='_blank' rel="noreferrer noopener">@ollinsalon</a>
                 </p>
@@ -77,3 +113,5 @@ export default function Contact() {
         </>
     )
 }
+
+export default Contact;
